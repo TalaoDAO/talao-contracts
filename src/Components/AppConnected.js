8c2635dbafe7483d340b029b5ec0c5e8b871b802
Sacha Leroux <sacha.leroux@slowsense.com>
2018-04-20 10:08:23 +0200

    Integrate Vault Factory Call

--- src/Components/AppConnected.js
+++ src/Components/AppConnected.js
@@ -17,25 +17,40 @@ class AppConnected extends React.Component {
           this.state = {
             vaultFactory: vaultFactory
           }
+
+          this.submit = this.submit.bind(this);
     }
 
     componentDidMount() {
-        this.state.vaultFactory.methods.getMyToken().call().then(token =>{
-            alert(token);
-        });
+        // this.state.vaultFactory.methods.getMyToken().call().then(token =>{
+        //     alert(token);
+        // });
 
-        this.state.vaultFactory.methods.CreateVaultContract().send({from:this.context.web3.selectedAccount}).then(addressVault => {
+        // this.state.vaultFactory.methods.CreateVaultContract().send({from:this.context.web3.selectedAccount}).then(addressVault => {
+        //     alert(addressVault);
+        // },e => {
+        //     alert(e);
+        // });
+        
+    }
+    
+    submit() {
+        //event.preventDefault();
+        this.state.vaultFactory.methods.CreateVaultContract().send(
+            {from:this.context.web3.selectedAccount, 
+                gas: 4700000, 
+                gasPrice:100000000000 }
+        ).then(addressVault => {
             alert(addressVault);
         },e => {
             alert(e);
         });
-        
     }
 
     render() {
         return (
             <div>
-                <button> Create Your Vault </button>
+                <button onClick={this.submit} > Create Your Vault </button>
                 <button> Add file to vault </button>
                 <p> {this.context.web3.selectedAccount}, {window.web3.defaultAccount}</p>
             </div>
