b9b59938e7b5fb5bad2dd7e975aacf9a3cb5e49c
Sacha Leroux <sacha.leroux@slowsense.com>
2018-04-23 12:32:26 +0200

    state correction

--- src/vault/Vault.js
+++ src/vault/Vault.js
@@ -9,21 +9,29 @@ class Vault extends React.Component {
         vaultFactoryContract: null,
         vaultContract : null,
         createVaultButton: true,
+        documents: [],
     }
 
     //connexion to 
     constructor(props) {
         super(props);
         //const contract
-        const vaultFactoryContract = new window.web3.eth.Contract(
+        const vaultFactoryCont = new window.web3.eth.Contract(
             JSON.parse(process.env.REACT_APP_VAULTFACTORY_ABI),
             process.env.REACT_APP_VAULTFACTORY_ADDRESS
         );
 
-        this.setState({vaultFactoryContract:vaultFactoryContract});
+        this.state = {
+            vaultFactoryContract: vaultFactoryCont,
+            vaultContract : null,
+            createVaultButton: true,
+            documents: [],
+        }
 
-        this.submit = this.submit.bind(this);
-        this.submit = this.submit.bind(this);
+        //this.setState({vaultFactoryContract:vaultFactoryCont});
+
+        this.createFreelanceVault = this.createFreelanceVault.bind(this);
+        this.addDocument = this.addDocument.bind(this);
     }
 
     componentDidMount() {
@@ -38,6 +46,8 @@ class Vault extends React.Component {
                 this.setState({
                     vaultContract: vaultContract
                 });
+
+
             } else {this.setState({
                 createVaultButton:false})
             }
@@ -59,7 +69,32 @@ class Vault extends React.Component {
     }
     
     addDocument() {
+        //add value on this call
+        this.state.vaultContract.methods.addDocument().send(
+        {
+            from: this.context.web3.selectedAccount,
+            gas: 4700000,
+            gasPrice: 100000000000
+        })
+        .on('transactionHash', hash => {
+            alert(hash);
+            this.setState({createVaultButton:false});
+        })
+        .on('error', (error) => { alert(error) });
+    }
 
+    removeDocument() {
+        this.state.vaultContract.methods.removeDocument().send(
+        {
+            from: this.context.web3.selectedAccount,
+            gas: 4700000,
+            gasPrice: 100000000000
+        })
+        .on('transactionHash', hash => {
+            alert(hash);
+            this.setState({createVaultButton:false});
+        })
+        .on('error', (error) => { alert(error) });
     }
 
     
