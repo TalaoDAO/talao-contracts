ce029c5bf8dab0cf43137c188e69e037e297af9c
Yann Faure <yann.faure@slowsense.com>
2018-04-23 16:44:46 +0200

    Ajout document

--- src/vault/Vault.js
+++ src/vault/Vault.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Button from '../ui/button/Button';
-import faBan from '@fortawesome/fontawesome-free-solid/faBan';
+import faPlus from '@fortawesome/fontawesome-free-solid/faPlus';
+import faFolder from '@fortawesome/fontawesome-free-solid/faFolder';
+import faCheck from '@fortawesome/fontawesome-free-solid/faCheck';
 import './Vault.css';
 
 class Vault extends React.Component {
@@ -20,17 +22,20 @@ class Vault extends React.Component {
             vaultContract: null,
             canCreateVault: false,
             documents: [],
+            view: 'vault',
         }
 
         //this.setState({vaultFactoryContract:vaultFactoryCont});
 
         this.createFreelanceVault = this.createFreelanceVault.bind(this);
         this.addDocument = this.addDocument.bind(this);
+        this.goToAddDocument = this.goToAddDocument.bind(this);
+        this.goToVault = this.goToVault.bind(this);
     }
 
     componentDidMount() {
         this.state.vaultFactoryContract.methods.FreelanceVault(this.context.web3.selectedAccount).call().then(vaultAdress => {
-            if (vaultAdress != '0x0000000000000000000000000000000000000000') {
+            if (vaultAdress !== '0x0000000000000000000000000000000000000000') {
 
                 const vaultContract = new window.web3.eth.Contract(
                     JSON.parse(process.env.REACT_APP_VAULT_ABI),
@@ -74,7 +79,7 @@ class Vault extends React.Component {
             })
             .on('transactionHash', hash => {
                 alert(hash);
-                this.setState({ canCreateVault: false });
+                this.setState({ canCreateVault: false, view: 'vault' });
             })
             .on('error', (error) => { alert(error) });
     }
@@ -93,23 +98,66 @@ class Vault extends React.Component {
             .on('error', (error) => { alert(error) });
     }
 
+    goToAddDocument() {
+        this.setState({ view: 'add-document' });
+    }
 
+    goToVault() {
+        this.setState({ view: 'vault' });
+    }
 
     render() {
         return (
-            <div className="vault">
-                <h1>My account</h1>
-                <p>Talent Etherum Address: {this.context.web3.selectedAccount}</p>
-                <p><img className="flash-code" /></p>
-                <div className="box blue m20">
-                    <p className="big">
-                        <span style={this.state.canCreateVault ? {} : { display: 'none' }}>
-                            <Button value="Create Your Vault" icon={faBan} onClick={this.createFreelanceVault} />
-                        </span>
-                        <span style={this.state.canCreateVault ? { display: 'none' } : {}}>
-                            <Button value="Add file to vault" icon={faBan} onClick={this.addDocument} />
-                        </span>
-                    </p>
+            <div>
+                <div className="vault" style={this.state.view === 'vault' ? {} : { display: 'none' }}>
+                    <h1>My account</h1>
+                    <p>Talent Etherum Address: {this.context.web3.selectedAccount}</p>
+                    <p><img className="flash-code" /></p>
+                    <div className="box blue m20">
+                        <p className="big">
+                            <span style={this.state.canCreateVault ? {} : { display: 'none' }}>
+                                To start, you must create a vault<br />
+                                <Button value="Create Your Vault" icon={faFolder} onClick={this.createFreelanceVault} />
+                            </span>
+                            <span style={this.state.canCreateVault ? { display: 'none' } : {}}>
+                                To continue, we need to verify your identify: <br />
+                                please upload your ID card, passport or driver license<br />
+                                <Button value="Add your ID document" icon={faPlus} onClick={this.goToAddDocument} />
+                            </span>
+                            <span style={this.state.documents.length === 0 ? { display: 'none' } : {}}>
+                                documents!
+                                </span>
+                        </p>
+                    </div>
+                </div>
+
+                <div className="add-document" style={this.state.view === 'add-document' ? {} : { display: 'none' }}>
+                    <h1>Add reference</h1>
+                    <p>Add a reference to your vault</p>
+                    <div className="box yellow m20">
+                        <div>
+                            <label htmlFor="description">Description</label>
+                        </div>
+                        <div>
+                            <textarea id="description" />
+                        </div>
+                        <div>
+                            <label htmlFor="keywords">Keywords</label>
+                        </div>
+                        <div>
+                            <textarea id="keywords" />
+                        </div>
+                        <div>
+                            <label htmlFor="document">Upload your references certification</label>
+                        </div>
+                        <div>
+                            <input type="file" id="document" />
+                        </div>
+                        <div>
+                            <Button value="Cancel" icon={faCheck} onClick={this.goToVault} />
+                            <Button value="Validate" icon={faCheck} onClick={this.addDocument} />
+                        </div>
+                    </div>
                 </div>
             </div>
         )
