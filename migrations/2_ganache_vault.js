1498eb5589cf66a0528105531bd213c946edfc62
Sacha Leroux <sacha.leroux@slowsense.com>
2018-04-23 11:24:56 +0200

    Correction of problem with vault

--- migrations/2_ganache_vault.js
+++ migrations/2_ganache_vault.js
@@ -1,13 +1,13 @@
-var OwnableContract = artifacts.require("Ownable");
+//var OwnableContract = artifacts.require("Ownable");
 var TalaoTokenContract = artifacts.require("TalaoToken");
 var VaultfactoryContract = artifacts.require("VaultFactory");
-var VaultContract = artifacts.require("Vault");
-var SafeMathLib = artifacts.require("SafeMath");
+//var VaultContract = artifacts.require("Vault");
+//var SafeMathLib = artifacts.require("SafeMath");
 
 module.exports = function(deployer) {
   // deployment steps
-  deployer.deploy(SafeMathLib);
-  deployer.deploy(OwnableContract);
+  //deployer.deploy(SafeMathLib);
+  //deployer.deploy(OwnableContract);
   deployer.deploy(TalaoTokenContract).then ( function() {
     deployer.deploy(VaultfactoryContract,TalaoTokenContract.address);
     //deployer.deploy(VaultContract, TalaoTokenContract.address);
--- src/vault/Vault.js
+++ src/vault/Vault.js
@@ -5,70 +5,64 @@ import faBan from '@fortawesome/fontawesome-free-solid/faBan';
 
 class Vault extends React.Component {
 
+    state = {
+        vaultFactoryContract: null,
+        vaultContract : null,
+        createVaultButton: true,
+    }
+
     //connexion to 
     constructor(props) {
         super(props);
         //const contract
-        const vaultFactory = new window.web3.eth.Contract(
+        const vaultFactoryContract = new window.web3.eth.Contract(
             JSON.parse(process.env.REACT_APP_VAULTFACTORY_ABI),
             process.env.REACT_APP_VAULTFACTORY_ADDRESS
         );
 
-        this.state = {
-            vaultFactory: vaultFactory,
-        }
+        this.setState({vaultFactoryContract:vaultFactoryContract});
 
+        this.submit = this.submit.bind(this);
         this.submit = this.submit.bind(this);
     }
 
     componentDidMount() {
+        this.state.vaultFactoryContract.methods.FreelanceVault(this.context.web3.selectedAccount).call().then(vaultAdress => {
+            if(vaultAdress!=null) {
+ 
+                const vaultContract = new window.web3.eth.Contract(
+                    JSON.parse(process.env.REACT_APP_VAULT_ABI),
+                    this.state.vaultAddress
+                );
+
+                this.setState({
+                    vaultContract: vaultContract
+                });
+            } else {this.setState({
+                createVaultButton:false})
+            }
+        })  
+    }
+
+    createFreelanceVault() {
+        this.state.vaultFactoryContract.methods.CreateVaultContract().send(
+        {
+            from: this.context.web3.selectedAccount,
+            gas: 4700000,
+            gasPrice: 100000000000
+        })
+        .on('transactionHash', hash => {
+            alert(hash);
+            this.setState({createVaultButton:false});
+        })
+        .on('error', (error) => { alert(error) });
+    }
+    
+    addDocument() {
 
     }
 
-    submit() {
-        this.state.vaultFactory.methods.CreateVaultContract().send(
-            {
-                from: this.context.web3.selectedAccount,
-                gas: 4700000,
-                gasPrice: 100000000000
-            })
-            .on('transactionHash', hash => {
-                alert(hash);
-            })
-            .on('error', (error) => { alert(error) });
-
-<<<<<<< HEAD:src/Components/Vault.js
-        // this.state.vaultFactory.methods.getMyToken()
-        //     .send({from:this.context.web3.selectedAccount,  gas: 4700000,gasPrice:100000000000 })
-        //     .on('transactionHash', (hash)=> {
-        //         console.log(hash);
-        //     })
-        //     .on('receipt', (receipt) => {
-        //         //alert(receipt);
-        //     })
-        //     .on('confirmation', (confirmation) => {
-        //         //alert(confirmation);
-        //     })
-        //     .on('error', (error) => {console.log(error)}); 
-=======
-        this.state.vaultFactory.methods.getMyToken()
-            .send({ from: this.context.web3.selectedAccount, gas: 4700000, gasPrice: 100000000000 })
-            .on('transactionHash', (hash) => {
-                console.log(hash);
-            })
-            .on('receipt', (receipt) => {
-                //alert(receipt);
-            })
-            .on('confirmation', (confirmation) => {
-                //alert(confirmation);
-            })
-            .on('error', (error) => { console.log(error) });
->>>>>>> 9700ff070aebaaf88ec1fc5fd8362f3de9468185:src/vault/Vault.js
-
-        // this.state.vaultFactory.methods.getNbVault().call().then(nb => {
-        //     alert(nb);
-        // });
-    }
+    
 
     render() {
         return (
@@ -77,8 +71,8 @@ class Vault extends React.Component {
                 <p>Talent Etherum Address: {this.context.web3.selectedAccount}</p>
                 <div className="box blue">
                     <p className="big">
-                        <Button value="Create Your Vault" icon={faBan} onClick={this.submit} />
-                        <Button value="Add file to vault" icon={faBan} />
+                        <Button className={this.state.createVaultButton ? 'hidden' : ''} value="Create Your Vault" icon={faBan} onClick={this.createFreelanceVault} />
+                        <Button value="Add file to vault" icon={faBan} onClick={this.addDocument} />
                     </p>
                 </div>
             </div>
