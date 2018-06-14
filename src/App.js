a3b3ea16778fb2bf24fed334bf95e657f064fb5d
Yann Faure <yann.faure@slowsense.com>
2018-06-14 16:13:12 +0200

    Start to switch to new UI

--- src/App.js
+++ src/App.js
@@ -1,35 +1,24 @@
 import React from 'react';
-import AppConnected from './AppConnected';
-import Web3Wrapper from './web3wrapper/Web3Wrapper';
-import logo from './assets/images/Talao.svg';
-import './App.css';
+import './App.scss';
+import Button from '@material/react-button/dist';
 
 class App extends React.Component {
 
     render() {
-        return (
-            <div className="App">
-            <header className="App-header white">
-              <a href="/">
-                <img src={ logo } className="App-header-logo" alt="logo" />
-              </a>
-            </header>
-            <section className="App-main white">
-              <Web3Wrapper>
-                <AppConnected />
-              </Web3Wrapper>
-            </section>
-            <footer className="App-footer green">
-              <p>This is our work in progress prototype, stay tuned for new features added on a regular basis.</p>
-              <ul className="App-footer-links">
-                <li><a href="https://github.com/TalaoDAO" target="_blank" rel="noopener noreferrer">GitHub</a></li>
-                <li><a href="https://ico.talao.io" target="_blank" rel="noopener noreferrer">The Talao ICO</a></li>
-                <li><a href="https://talao.io" target="_blank" rel="noopener noreferrer">Talao.io</a></li>
-              </ul>
-              <p>v0.9</p>
-            </footer>
-          </div>
-            );
+      return (
+        <div>
+          <header>
+            Header
+          </header>
+          <main>
+            <Button className='button-alternate'>
+              Hello World
+            </Button>
+          </main>
+          <footer>
+          </footer>
+        </div>
+      );
     }
 }
 
