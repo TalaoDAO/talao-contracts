95dc6e876066e1782fc57a1adbf4a193455eb6c5
Yann Faure <yann.faure@slowsense.com>
2018-06-27 16:36:15 +0200

    Start of competency content

--- src/components/competency/Competency.js
+++ src/components/competency/Competency.js
@@ -37,9 +37,16 @@ const styles = {
     },
     card: {
         transition: 'all .2s ease',
+    },
+    cardnormal: {
         '&:hover': {
             boxShadow: '0px 3px 15px 0px rgba(0, 0, 0, 0.2), 0px 6px 6px 0px rgba(0, 0, 0, 0.14), 0px 9px 3px -6px rgba(0, 0, 0, 0.12)',
             cursor: 'pointer',
+        },
+    },
+    cardfocused: {
+        '&:hover': {
+            cursor: 'initial',
         },
     },
     link: {
@@ -47,12 +54,29 @@ const styles = {
         color: 'initial',
     },
     competentiesHidden: {
-        height: 0,
+        maxHeight: '0px',
         overflow: 'hidden',
     },
     competentiesShown: {
-        height: 'auto',
-        transition: 'all .5s ease',
+        maxHeight: '0px',
+        overflow: 'auto',
+        animationName: 'openExperiences',
+        animationDelay: '.5s',
+        animationDuration: '1s',
+        animationFillMode: 'forwards',
+    },
+    '@keyframes openExperiences': {
+        '0%': {
+            maxHeight: '0px',
+            overflow: 'hidden',
+        },
+        '99%': {
+            maxHeight: '5000px',
+        },
+        '100%': {
+            height: 'auto',
+            maxHeight: 'initial',
+        },
     },
 };
 
@@ -66,50 +90,50 @@ class Competency extends React.Component {
         return "accent4";
     }
 
-render() {
-    const backgroundColorString = this.getCompetencyColor();
-    const backgroundLightColorString = "light" + backgroundColorString[0].toUpperCase() + backgroundColorString.substring(1);
-    const layoutFocused = this.props.layout === 'focused';
-    return (
-        <Link to={'/competencies/' + this.props.competency.name} className={this.props.classes.link}>
-            <Card className={this.props.classes.card}>
-                <CardContent className={this.props.classes.badge} style={{ backgroundColor: constants.colors[backgroundLightColorString]}}>
-                    <Typography variant="headline" className={this.props.classes.textBadge}>
-                        {Math.round(this.props.confidenceIndex)}
-                    </Typography>
-                </CardContent>
-                <CardMedia
-                    className={this.props.classes.media}
-                    style={{ backgroundColor: constants.colors[backgroundColorString] }}
-                    image="#"
-                    title={this.props.competency.name}
-                />
-                <CardContent
-                    className={this.props.classes.content}>
-                    <Typography gutterBottom variant="headline" component="h2" className={this.props.classes.competency}>
-                        {this.props.competency.name}
-                    </Typography>
-                    <div className={layoutFocused ? this.props.classes.competentiesShown : this.props.classes.competentiesHidden}>
-                        <p>test</p>
-                        <p>test</p>
-                        <p>test</p>
-                        <p>test</p>
-                        <p>test</p>
-                        <p>test</p>
-                        <p>test</p>
-                        <p>test</p>
-                        <p>test</p>
-                    </div>
-                </CardContent>
-                <CardActions style={{display: layoutFocused ? 'none' : 'initial'}}>
-                    <Button size="small" color="primary">
-                        Details
+    render() {
+        const backgroundColorString = this.getCompetencyColor();
+        const backgroundLightColorString = "light" + backgroundColorString[0].toUpperCase() + backgroundColorString.substring(1);
+        const layoutFocused = this.props.layout === 'focused';
+        return (
+            <Link to={'/competencies/' + this.props.competency.name} className={this.props.classes.link}>
+                <Card className={this.props.classes.card + ' ' + this.props.classes['card' + this.props.layout]}>
+                    <CardContent className={this.props.classes.badge} style={{ backgroundColor: constants.colors[backgroundLightColorString] }}>
+                        <Typography variant="headline" className={this.props.classes.textBadge}>
+                            {Math.round(this.props.confidenceIndex)}
+                        </Typography>
+                    </CardContent>
+                    <CardMedia
+                        className={this.props.classes.media}
+                        style={{ backgroundColor: constants.colors[backgroundColorString] }}
+                        image="#"
+                        title={this.props.competency.name}
+                    />
+                    <CardContent
+                        className={this.props.classes.content}>
+                        <Typography gutterBottom variant="headline" component="h2" className={this.props.classes.competency}>
+                            {this.props.competency.name}
+                        </Typography>
+                        <div className={layoutFocused ? this.props.classes.competentiesShown : this.props.classes.competentiesHidden}>
+                            <p>test</p>
+                            <p>test</p>
+                            <p>test</p>
+                            <p>test</p>
+                            <p>test</p>
+                            <p>test</p>
+                            <p>test</p>
+                            <p>test</p>
+                            <p>test</p>
+                        </div>
+                    </CardContent>
+                    <CardActions style={{ display: layoutFocused ? 'none' : 'initial' }}>
+                        <Button size="small" color="primary">
+                            Details
                     </Button>
-                </CardActions>
-            </Card>
-        </Link>
-    );
-}
+                    </CardActions>
+                </Card>
+            </Link>
+        );
+    }
 }
 
 export default withStyles(styles)(Competency);
