d3a2cb1d5d5d129021acf0e89286fba9a9919e02
Pierre-Henri <pierre-henri.debris@slowsense.com>
2018-08-29 08:20:13 +0200

    SC modified  - Load and upload profile picture from SC & IPFS  - Load and upload ratings to IPFS / SC and Read it  - Fix the front to match the new SC security rules

--- src/actions/guard.js
+++ src/actions/guard.js
@@ -58,12 +58,12 @@ export function hasAccess(location, params, user, history) {
         //Valid parameter
         else {
 
-            //The vault price of the freelancer is 0 talao token ??
-            let accessPriceIsZeroTalaoToken;
-            user.talaoContract.methods.data(params).call().then(info => {
-                accessPriceIsZeroTalaoToken = (parseInt(window.web3.utils.fromWei(info.accessPrice), 10) === 0 ) ? true : false;
-            }).then(() => {
-
+            //Check if the searchedFreelancer is a freelancer
+            userHasAccessToFreelancer(user, params).then((resolve, reject) => {
+            let hasVault = (!resolve) ? false : true;
+            let userHasAccess = (!resolve || resolve === '0x0000000000000000000000000000000000000000') ? false : true;
+            console.log(hasVault);
+            console.log(userHasAccess);
             //the client doesn't have a wallet
             if (!user.ethAddress) {
                 switch (location.toLowerCase()) 
@@ -71,7 +71,7 @@ export function hasAccess(location, params, user, history) {
                     case 'chronology':
                     case 'competencies':
                         //if the vault is free : authorize
-                        if (!accessPriceIsZeroTalaoToken)
+                        if (!userHasAccess)
                             redirectTo = guardHasError(dispatch, 'This vault is not free ! Please log in to unlock this freelancer', '/homepage');         
                         break;
                     case 'unlockfreelancer': 
@@ -121,52 +121,33 @@ export function hasAccess(location, params, user, history) {
 
                 //The user is a client looking for a freelancer
                 } else {
-                    //The user has a wallet and he is looking for someonelse, and the parameter is valid
-                    let isPartner = null;
-                    let hasVaultAccess = null;
-                    let hasVault = null;
-                    //Check if user has a vault
-                    user.vaultFactoryContract.methods.FreelanceVault(params).call().then(vaultAddress => {
-                        hasVault = (vaultAddress === '0x0000000000000000000000000000000000000000') ? false : true;
-                    }).then(() => {
-                        // This freelancer is a partner ??
-                        user.freelancerContract.methods.isPartner(params, user.ethAddress).call().then(partner => {
-                            isPartner = partner;
-                        }).then(() => {
-                            // This client has already unlock the freelancer vault ??
-                            user.talaoContract.methods.hasVaultAccess(params, user.ethAddress).call().then(hasAccessToFreelanceVault => {
-                                hasVaultAccess = hasAccessToFreelanceVault;
-                            }).then(() => {
-                                switch (location.toLowerCase()) 
-                                {
-                                    case 'chronology':
-                                    case 'competencies':
-                                        if (!hasVault) {
-                                            redirectTo = guardHasError(dispatch, 'This freelancer doesn\'t have a vault.', '/homepage');         
-                                        } else if (!isPartner && !hasVaultAccess && !accessPriceIsZeroTalaoToken) {
-                                            //Access needed, so redirect to unlockfreelancer
-                                            redirectTo = '/unlockfreelancer?' + params;
-                                            dispatch(changeMenu('/unlockfreelancer'));
-                                        }
-                                        break;
-                                    case 'unlockfreelancer':
-                                        if (!hasVault) {
-                                            redirectTo = guardHasError(dispatch, 'This freelancer doesn\'t have a vault.', '/homepage');      
-                                        } else if (isPartner || hasVaultAccess || accessPriceIsZeroTalaoToken) {
-                                            //The user has access, so redirect him directly on the chronology of the searchedfreelancer
-                                            redirectTo = '/chronology?' + params;
-                                            dispatch(changeMenu('/chronology'));
-                                        }
-                                        break
-                                    case 'register':
-                                        redirectTo = guardHasError(dispatch, 'You can\'t edit this vault.', '/homepage');      
-                                        break;
-                                    default: redirectTo = '/homepage';
-                                }
-                                (redirectTo) ? history.push(redirectTo) : dispatch(guardValid());
-                            });
-                        });
-                    });
+                    switch (location.toLowerCase()) 
+                    {
+                        case 'chronology':
+                        case 'competencies':
+                            if (!hasVault) {
+                                redirectTo = guardHasError(dispatch, 'This freelancer doesn\'t have a vault.', '/homepage');         
+                            } else if (!userHasAccess) {
+                                //Access needed, so redirect to unlockfreelancer
+                                redirectTo = '/unlockfreelancer?' + params;
+                                dispatch(changeMenu('/unlockfreelancer'));
+                            }
+                            break;
+                        case 'unlockfreelancer':
+                            if (!hasVault) {
+                                redirectTo = guardHasError(dispatch, 'This freelancer doesn\'t have a vault.', '/homepage');      
+                            } else if (userHasAccess) {
+                                //The user has access, so redirect him directly on the chronology of the searchedfreelancer
+                                redirectTo = '/chronology?' + params;
+                                dispatch(changeMenu('/chronology'));
+                            }
+                            break
+                        case 'register':
+                            redirectTo = guardHasError(dispatch, 'You can\'t edit this vault.', '/homepage');      
+                            break;
+                        default: redirectTo = '/homepage';
+                    }
+                    (redirectTo) ? history.push(redirectTo) : dispatch(guardValid());
                 }
             }});
         }
@@ -177,4 +158,20 @@ export function guardHasError(dispatch, message, redirection) {
     dispatch(guardError(message));
     dispatch(changeMenuClicked(redirection, false));
     return redirection;
+}
+
+export function userHasAccessToFreelancer(user, freelancer) {
+    return new Promise((resolve, reject) => {
+        user.vaultFactoryContract.methods.HasVault(freelancer).call().then(hasVault => {
+            if (hasVault) {
+                user.vaultFactoryContract.methods.GetVault(freelancer).call({from: user.ethAddress}).then(vaultAddress => {
+                    resolve(vaultAddress);
+                });
+            } else {
+                resolve(false);
+            }
+        }).catch((err) => {
+            reject(err);
+        });
+    });
 }
