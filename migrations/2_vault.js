d56f3bc2e9344f26d424c9671429051a2afbd038
Guillaume Duveau <guillaume.duveau@gmail.com>
2018-12-12 13:33:55 +0100

    cleanup

--- migrations/2_vault.js
+++
@@ -1,8 +0,0 @@
-const TalaoTokenContract = artifacts.require('./TalaoToken');
-const VaultFactoryContract = artifacts.require('./VaultFactory');
-
-module.exports = function (deployer) {
-  deployer.deploy(TalaoTokenContract).then(function () {
-    return deployer.deploy(VaultFactoryContract, TalaoTokenContract.address);
-  });
-};
