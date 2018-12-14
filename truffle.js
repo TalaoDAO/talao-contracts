e575493cef72d18b6177deda51a1c9be43ed3998
Guillaume Duveau <guillaume.duveau@gmail.com>
2018-12-14 17:02:42 +0100

    cleanup

--- truffle.js
+++ truffle.js
@@ -1,37 +1,17 @@
-// Comment those lines if you do not want to deploy with Truffle + Infura on Ropsten.
-// To use it you must npm install truffle-hdwallet-provider
-const secret = require('./secret');
-const mnemonic = secret.mnemonic();
-const infuraApiKey = secret.infuraApiKey();
-const HDWalletProvider = require('truffle-hdwallet-provider');
-
-require('babel-register');
-require('babel-polyfill');
+/*
+ * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a
+ * function when declaring them. Failure to do so will cause commands to hang. ex:
+ * ```
+ * mainnet: {
+ *     provider: function() {
+ *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>')
+ *     },
+ *     network_id: '1',
+ *     gas: 4500000,
+ *     gasPrice: 10000000000,
+ *   },
+ */
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
-  networks: {
-    ganache: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: 3
-    },
-    parity: {
-      host: "127.0.0.1",
-      port: 8180,
-      network_id: "*" // matching any id
-    },
-    testrpc: {
-      host: "127.0.0.1",
-      port: 8545,
-      network_id: "*" // matching any id
-    },
-    ropsten: {
-      provider: function() {
-        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/" + infuraApiKey, 5)
-      },
-      network_id: 3
-    }
-  }
-};
