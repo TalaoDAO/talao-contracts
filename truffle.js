46ade24aafed9d2466b8a1855369a63543a67c3f
Guillaume Duveau <guillaume.duveau@gmail.com>
2018-12-15 20:37:08 +0100

    ganache-cli

--- truffle.js
+++ truffle.js
@@ -15,4 +15,11 @@
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+  networks: {
+    ganachecli: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "11"
+    }
+  }
 }
