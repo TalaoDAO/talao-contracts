05c535dd60e696b139f11276fd539804fbfe6fac
Guillaume Duveau <guillaume.duveau@gmail.com>
2018-12-15 14:46:07 +0100

    test/Tokenized.js

--- contracts/Tokenized.sol
+++ contracts/Tokenized.sol
@@ -9,14 +9,19 @@ import './TalaoToken.sol';
  */
 contract Tokenized is Partnership {
 
+    // Interface with Foundation.
+    FoundationInterface foundation;
+
+    // TODO: interface instead.
     TalaoToken public token;
 
     /**
      * @dev Constructor.
      */
-    constructor(uint8 _partnerCategory, address _token)
-        public Partnership(_partnerCategory)
+    constructor(address _foundation, uint8 _partnerCategory, address _token)
+        public Partnership(_foundation, _partnerCategory)
     {
+        foundation = FoundationInterface(_foundation);
         partnerCategory = _partnerCategory;
         token = TalaoToken(_token);
     }
@@ -25,9 +30,9 @@ contract Tokenized is Partnership {
      * @dev Read authorization for inherited contracts "private" data.
      */
     function isReader() public view returns (bool) {
-        // Get Vault access price in the token.
-        (uint accessPrice,,,) = token.data(owner());
-
+        // Get Vault access price in the token for this contract owner,
+        // in the Foundation sense.
+        (uint accessPrice,,,) = token.data(ownerInFoundation());
         // OR conditions for Reader:
         // 1) Sender is this contract owner.
         // 2) Sender has vaultAccess to this contract owner in the token.
@@ -36,15 +41,15 @@ contract Tokenized is Partnership {
         // 4) The owner has a free vaultAccess in the token and
         // has an open Vault in the token.
         return(
-            isOwner() ||
-            token.hasVaultAccess(owner(), msg.sender) ||
+            isOwnerInFoundation() ||
+            token.hasVaultAccess(ownerInFoundation(), msg.sender) ||
             (
                 isAuthorizedPartnershipOwner() &&
-                token.hasVaultAccess(owner(), owner())
+                token.hasVaultAccess(ownerInFoundation(), ownerInFoundation())
             ) ||
             (
                 accessPrice == 0 &&
-                token.hasVaultAccess(owner(), owner())
+                token.hasVaultAccess(ownerInFoundation(), ownerInFoundation())
             )
         );
     }
