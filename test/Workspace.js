dc9bbbad4e6c88fd5d3432bc99ceac5383e135cd
Guillaume Duveau <guillaume.duveau@gmail.com>
2018-12-29 17:45:56 +0100

    remove test duplicates

--- test/Workspace.js
+++ test/Workspace.js
@@ -1,10 +1,15 @@
+const web3 = require('web3');
 const truffleAssert = require('truffle-assertions');
 
+// Contract artifacts.
 const KeyHolderLibrary = artifacts.require('./identity/KeyHolderLibrary.sol');
 const ClaimHolderLibrary = artifacts.require('./identity/ClaimHolderLibrary.sol');
 const TalaoToken = artifacts.require('TalaoToken');
 const Foundation = artifacts.require('Foundation');
 const Workspace = artifacts.require('Workspace');
+
+// Contract instances.
+let token, foundation, workspace;
 
 // "this string just fills a bytes32"
 let name1, name2, tagline, url, publicEmail, privateEmail, fileHash;
@@ -16,28 +21,24 @@ const description = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Al
 const fileEngine = 1;
 
 contract('Workspace', async (accounts) => {
-  const talaoOwner = accounts[0];
+  const defaultUser = accounts[0];
   const user1 = accounts[1];
   const user2 = accounts[2];
   const user3 = accounts[3];
   const factory = accounts[8];
   const someone = accounts[9];
-  let token;
-  let foundation;
-  let workspace1, workspace2;
 
-  it('Should deploy keyHolderLibrary, link it in ClaimHolderLibrary, deploy claimHolderLibrary, link both libs in Profile', async() => {
+  // Init.
+  before(async () => {
+    // 1. Deploy & link librairies.
     keyHolderLibrary = await KeyHolderLibrary.new();
     await ClaimHolderLibrary.link(KeyHolderLibrary, keyHolderLibrary.address);
     claimHolderLibrary = await ClaimHolderLibrary.new();
     await Workspace.link(KeyHolderLibrary, keyHolderLibrary.address);
     await Workspace.link(ClaimHolderLibrary, claimHolderLibrary.address);
-  });
-
-  // Simple init, already fully tested before the ICO.
-  it('Should init token with Vault deposit of 100 TALAO and transfer 1000 TALAO to User1, User2 and User3. User1 should create a Vault access with a price of 10 TALAO and User2 should create a free Vault access', async() => {
+    // 2. Deploy Talao token, set it, transfer TALAOs and open Vault access.
     token = await TalaoToken.new();
-    await token.mint(talaoOwner, 150000000000000000000);
+    await token.mint(defaultUser, 150000000000000000000);
     await token.finishMinting();
     await token.setVaultDeposit(100);
     await token.transfer(user1, 1000);
@@ -45,18 +46,14 @@ contract('Workspace', async (accounts) => {
     await token.transfer(user3, 1000);
     await token.createVaultAccess(10, { from: user1 });
     await token.createVaultAccess(0, { from: user2 });
-  });
-
-  // Already tested in Foundation.js.
-  it('Should deploy Foundation contract and register a Factory contract', async() => {
+    await token.createVaultAccess(50, { from: user3 });
+    // 3. Deploy Foundation & register a Factory.
     foundation = await Foundation.new();
-    // It's only a simulation of a factory contract, otherwise I would have to create one just for this test.
     await foundation.addFactory(factory);
   });
 
-  // Simple init for initial owners, already tested in OwnableInFoundation.js
-  it('Factory should deploy Workspace1 (category 1001 = Freelancer) and Workspace2 (category 2001 = Marketplace) and set initial owners to User1 and User2', async() => {
-    workspace1 = await Workspace.new(
+  it('Factory should deploy Workspace (category 1001 = Freelancer), set initial owner and give him ERC 725 Management key', async() => {
+    workspace = await Workspace.new(
       foundation.address,
       token.address,
       1001,
@@ -66,25 +63,12 @@ contract('Workspace', async (accounts) => {
       '0x',
       {from: factory}
     );
-    assert(workspace1);
-    await foundation.setInitialOwnerInFoundation(workspace1.address, user1, {from: factory});
-    workspace2 = await Workspace.new(
-      foundation.address,
-      token.address,
-      2001,
-      0,
-      0,
-      '0x',
-      '0x',
-      {from: factory}
-    );
-    assert(workspace2);
-    await foundation.setInitialOwnerInFoundation(workspace2.address, user2, {from: factory});
+    assert(workspace);
+    await foundation.setInitialOwnerInFoundation(workspace.address, user1, {from: factory});
+    const user1key = web3.utils.keccak256(user1);
+    await workspace.addKey(user1key, 1, 1, {from: factory});
   });
 
-  it('Workspace 1 should have Factory as creator', async() =>  {
-    const result = await workspace1.identityInformation();
-    assert.equal(result[0], factory);
-  });
+  // TODO: kill
 
 });
