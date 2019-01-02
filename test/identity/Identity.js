ec2199d4dad9a0827b15cc2fff4bff6f8aeeca35
Guillaume Duveau <guillaume.duveau@gmail.com>
2019-01-02 20:02:09 +0100

    addClaims for Profile

--- test/identity/Identity.js
+++ test/identity/Identity.js
@@ -3,6 +3,7 @@ const NodeRSA = require('node-rsa');
 const aesjs = require('aes-js');
 const web3 = require('web3');
 const truffleAssert = require('truffle-assertions');
+const erc735js = require('erc735js');
 
 // Contract artifacts.
 const KeyHolderLibrary = artifacts.require('./identity/KeyHolderLibrary.sol');
@@ -29,6 +30,17 @@ const textToEncrypt = 'Hi, here is a message for your eyes only!';
 const fileEngine = 1;
 const fileType = 12;
 const fileHash = '0x7468697320737472696e67206a7573742066696c6c7320612062797465733332';
+
+// "Profile" as ERC 735 self-claims.
+// See https://github.com/ethereum/EIPs/issues/735#issuecomment-450647097
+const profile = {
+  givenName: "John",
+  familyName: "Doe",
+  jobTitle: "Solidity developer",
+  url: "https://johndoe.com",
+  email: "john@doe.com",
+  description: "I love building dApps"
+}
 
 // Tests.
 contract('Identity', async (accounts) => {
@@ -272,6 +284,194 @@ contract('Identity', async (accounts) => {
   it('Anyone should see that Identity is active', async() => {
     const result = await identity.isActiveIdentity({from: someone});
     assert(result);
+  });
+
+  // Note: to fully test ClaimHolder, we also add signatures.
+  // However in the UI we won't add signatures for self-claims,
+  // because signing self-claims makes no sense.
+  it('User1 should add one self-claim', async() => {
+    const result = await identity.addClaim(
+      erc735js.asciiToTopic('givenName'),
+      1,
+      user1,
+      web3.utils.keccak256(identity.address, erc735js.asciiToTopic('givenName'), profile.givenName),
+      web3.utils.asciiToHex(profile.givenName),
+      'https://user1.com/about',
+      {from: user1}
+    );
+    assert(result);
+    truffleAssert.eventEmitted(result, 'ClaimAdded', (ev) => {
+      return ev.claimId === web3.utils.soliditySha3(user1, erc735js.asciiToTopic('givenName'));
+    });
+  });
+
+  it('Anyone should retrieve claim IDs by topic and claimIDs should be deterministic', async() => {
+    const result = await identity.getClaimIdsByTopic(erc735js.asciiToTopic('givenName'), {from: someone});
+    assert.equal(result[0], web3.utils.soliditySha3(user1, erc735js.asciiToTopic('givenName')));
+  });
+
+  it('Anyone should retrieve a claim by its ID', async() => {
+    const claimId = web3.utils.soliditySha3(user1, erc735js.asciiToTopic('givenName'));
+    const result = await identity.getClaim(claimId, {from: someone});
+    assert.equal(result[0].toNumber(), erc735js.asciiToTopic('givenName'));
+    assert.equal(result[1].toNumber(), 1);
+    assert.equal(result[2], user1);
+    assert.equal(result[3], web3.utils.keccak256(identity.address, erc735js.asciiToTopic('givenName'), profile.givenName));
+    assert.equal(web3.utils.hexToAscii(result[4]), profile.givenName);
+    assert.equal(result[5], 'https://user1.com/about');
+  });
+
+  it('User1 should remove a self-claim by its ID', async() => {
+    const claimId = web3.utils.soliditySha3(user1, erc735js.asciiToTopic('givenName'));
+    const result = await identity.removeClaim(claimId, {from: user1});
+    assert(result);
+    truffleAssert.eventEmitted(result, 'ClaimRemoved', (ev) => {
+      return ev.claimId === web3.utils.soliditySha3(user1, erc735js.asciiToTopic('givenName'));
+    });
+  });
+
+  it('User should not have a self-claim on this topic any more', async() => {
+    const claimId = web3.utils.soliditySha3(user1, erc735js.asciiToTopic('givenName'));
+    const result = await identity.getClaim(claimId, {from: someone});
+    assert.equal(result[0].toNumber(), 0);
+    assert.equal(result[1].toNumber(), 0);
+    assert.equal(result[2], '0x0000000000000000000000000000000000000000');
+    assert.equal(result[3], '0x');
+    assert.equal(result[4], '0x');
+    assert.equal(result[5], '');
+  });
+
+  // Note: to fully test ClaimHolder, we also add signatures.
+  // However in the UI we won't add signatures for self-claims,
+  // because signing self-claims makes no sense.
+  it('User1 should add self-claim with the addClaims function', async() => {
+    // For one sig row we convert to hex and remove 0x
+    const sigRow = web3.utils.keccak256(identity.address, erc735js.asciiToTopic('givenName'), profile.givenName).substr(2);
+    // Serialized sig is concatenation of sig rows + we add again 0x in the beginning.
+    const sig = '0x' + sigRow;
+    // For one data row we convert to hex and remove 0x
+    const dataRow = web3.utils.asciiToHex(profile.givenName).substr(2);
+    // Serialized data is concatenation of data rows + we add again 0x in the beginning.
+    const data = '0x' + dataRow;
+    const result = await identity.addClaims(
+      [
+        erc735js.asciiToTopic('givenName')
+      ],
+      [
+        user1
+      ],
+      sig,
+      data,
+      [
+        profile.givenName.length,
+      ],
+      {from: user1}
+    );
+    assert(result)
+  });
+
+  it('Claim added with addClaims should have correct data', async() => {
+    const claimId = web3.utils.soliditySha3(user1, erc735js.asciiToTopic('givenName'));
+    const result = await identity.getClaim(claimId);
+    assert.equal(web3.utils.hexToAscii(result[4]), profile.givenName);
+  });
+
+  it('User1 should set his "Profile" with ERC 735 self-claims in 1 call to addClaims', async() => {
+    // TODO picture
+    const result = await identity.addClaims(
+      [
+        erc735js.asciiToTopic('givenName'),
+        erc735js.asciiToTopic('familyName'),
+        erc735js.asciiToTopic('jobTitle'),
+        erc735js.asciiToTopic('url'),
+        erc735js.asciiToTopic('email'),
+        erc735js.asciiToTopic('description')
+      ],
+      [
+        user1,
+        user1,
+        user1,
+        user1,
+        user1,
+        user1
+      ],
+      '0x'
+      +  web3.utils.keccak256(identity.address, erc735js.asciiToTopic('givenName'), profile.givenName).substr(2)
+      +  web3.utils.keccak256(identity.address, erc735js.asciiToTopic('familyName'), profile.familyName).substr(2)
+      +  web3.utils.keccak256(identity.address, erc735js.asciiToTopic('jobTitle'), profile.jobTitle).substr(2)
+      +  web3.utils.keccak256(identity.address, erc735js.asciiToTopic('url'), profile.url).substr(2)
+      +  web3.utils.keccak256(identity.address, erc735js.asciiToTopic('email'), profile.email).substr(2)
+      +  web3.utils.keccak256(identity.address, erc735js.asciiToTopic('description'), profile.description).substr(2),
+      '0x'
+      + web3.utils.asciiToHex(profile.givenName).substr(2)
+      + web3.utils.asciiToHex(profile.familyName).substr(2)
+      + web3.utils.asciiToHex(profile.jobTitle).substr(2)
+      + web3.utils.asciiToHex(profile.url).substr(2)
+      + web3.utils.asciiToHex(profile.email).substr(2)
+      + web3.utils.asciiToHex(profile.description).substr(2),
+      [
+        profile.givenName.length,
+        profile.familyName.length,
+        profile.jobTitle.length,
+        profile.url.length,
+        profile.email.length,
+        profile.description.length
+      ],
+      {from: user1}
+    );
+    assert(result);
+  });
+
+  it('User1 self claims for his "Profile" should exist and have correct data', async() => {
+    let result;
+
+    result = await identity.getClaim(web3.utils.soliditySha3(user1, erc735js.asciiToTopic('givenName')));
+    assert.equal(result[0].toNumber(), erc735js.asciiToTopic('givenName'));
+    assert.equal(result[1].toNumber(), 1);
+    assert.equal(result[2], user1);
+    assert.equal(result[3], web3.utils.keccak256(identity.address, erc735js.asciiToTopic('givenName'), profile.givenName));
+    assert.equal(web3.utils.hexToAscii(result[4]), profile.givenName);
+    assert.equal(result[5], '');
+
+    result = await identity.getClaim(web3.utils.soliditySha3(user1, erc735js.asciiToTopic('familyName')));
+    assert.equal(result[0].toNumber(), erc735js.asciiToTopic('familyName'));
+    assert.equal(result[1].toNumber(), 1);
+    assert.equal(result[2], user1);
+    assert.equal(result[3], web3.utils.keccak256(identity.address, erc735js.asciiToTopic('familyName'), profile.familyName));
+    assert.equal(web3.utils.hexToAscii(result[4]), profile.familyName);
+    assert.equal(result[5], '');
+
+    result = await identity.getClaim(web3.utils.soliditySha3(user1, erc735js.asciiToTopic('jobTitle')));
+    assert.equal(result[0].toNumber(), erc735js.asciiToTopic('jobTitle'));
+    assert.equal(result[1].toNumber(), 1);
+    assert.equal(result[2], user1);
+    assert.equal(result[3], web3.utils.keccak256(identity.address, erc735js.asciiToTopic('jobTitle'), profile.jobTitle));
+    assert.equal(web3.utils.hexToAscii(result[4]), profile.jobTitle);
+    assert.equal(result[5], '');
+
+    result = await identity.getClaim(web3.utils.soliditySha3(user1, erc735js.asciiToTopic('url')));
+    assert.equal(result[0].toNumber(), erc735js.asciiToTopic('url'));
+    assert.equal(result[1].toNumber(), 1);
+    assert.equal(result[2], user1);
+    assert.equal(result[3], web3.utils.keccak256(identity.address, erc735js.asciiToTopic('url'), profile.url));
+    assert.equal(web3.utils.hexToAscii(result[4]), profile.url);
+    assert.equal(result[5], '');
+
+    result = await identity.getClaim(web3.utils.soliditySha3(user1, erc735js.asciiToTopic('email')));
+    assert.equal(result[0].toNumber(), erc735js.asciiToTopic('email'));
+    assert.equal(result[1].toNumber(), 1);
+    assert.equal(result[2], user1);
+    assert.equal(result[3], web3.utils.keccak256(identity.address, erc735js.asciiToTopic('email'), profile.email));
+    assert.equal(web3.utils.hexToAscii(result[4]), profile.email);
+    assert.equal(result[5], '');
+
+    result = await identity.getClaim(web3.utils.soliditySha3(user1, erc735js.asciiToTopic('description')));
+    assert.equal(result[0].toNumber(), erc735js.asciiToTopic('description'));
+    assert.equal(result[1].toNumber(), 1);
+    assert.equal(result[2], user1);
+    assert.equal(result[3], web3.utils.keccak256(identity.address, erc735js.asciiToTopic('description'), profile.description));
+    assert.equal(web3.utils.hexToAscii(result[4]), profile.description);
+    assert.equal(result[5], '');
   });
 
   it('User1 closes his Vault access in the token, he should not be the Active Identity Owner', async() => {
