adf73dc62b9a5b4371e67c5dc0ed5e9489b39eee
Guillaume Duveau <guillaume.duveau@gmail.com>
2019-01-06 22:36:31 +0100

    updatae

--- migrations/2_talaotoken.js
+++
@@ -1,7 +0,0 @@
-// Comment all to not deploy a new token.
-
-// const TalaoToken = artifacts.require('./token/TalaoToken');
-//
-// module.exports = function(deployer) {
-//   deployer.deploy(TalaoToken);
-// };
--- migrations/3_workspacefactory.js
+++ migrations/3_workspacefactory.js
@@ -6,14 +6,10 @@ const WorkspaceFactory = artifacts.require('./WorkspaceFactory.sol');
 
 module.exports = function(deployer) {
    deployer.deploy(Foundation, {overwrite: false});
-   // Comment this to not deploy a new token.
-   // deployer.deploy(TalaoToken, {overwrite: false});
    deployer.deploy(KeyHolderLibrary);
    deployer.link(KeyHolderLibrary, [ClaimHolderLibrary, WorkspaceFactory]);
    deployer.deploy(ClaimHolderLibrary);
    deployer.link(ClaimHolderLibrary, WorkspaceFactory);
-   // Comment this to not deploy a new token.
-   // deployer.deploy(WorkspaceFactory, Foundation.address, TalaoToken.address);
-   // Uncomment and fill in token address to not deploy a new token.
+   // TODO Mainnet: replace by Mainnet token.
    deployer.deploy(WorkspaceFactory, Foundation.address, '0xb8a0a9ee2e780281637bd93c13076cc5e342c9ae');
 };
